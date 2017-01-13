75ea50b2ea91ac2ceb0b76ae879a25fc6dfa4ffd
Benjamin T. Seaver <BenjaminTSeaver@gmail.com>
2017-01-13 11:50:22 -0800

    add questionnaire scoring function

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,23 @@
 // JavaScript to run after DOM has loaded
 $(document).ready(function() {
+  // Get array of scores from questionnaire
+  // Column 0 is the radio button's name
+  // Column 1 is the numeric value from the checked button
+  var getScores = function(scoreArray) {
+    var name, score;
+    $(".radio.questionnaireScore label input:checked").each(function() {
+      name = $(this).attr("name");
+      score = parseInt( $(this).val() );
+      scoreArray.push([name, score]);
+    });
+  };
 
   // Handle Form Submit
-  $("Form").submit(function(event) {
+  $("form").submit(function(event) {
+    var scoreArray = [];
+
+    getScores(scoreArray);
+    console.log(scoreArray);
 
     // Reveal the Suggestions
     $("#suggestions").removeClass("hidden");
