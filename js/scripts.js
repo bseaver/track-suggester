968260b9ef9280ea47b9bfe7a012fad4df19783d
Benjamin T. Seaver <BenjaminTSeaver@gmail.com>
2017-01-13 09:24:32 -0800

    remove alerts from scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,12 @@
 // JavaScript to run after DOM has loaded
 $(document).ready(function() {
-  alert("document ready");
 
   // Handle Form Submit
   $("Form").submit(function(event) {
-    alert("form submit");
 
     // Reveal the Suggestions
     $("#suggestions").removeClass("hidden");
-    
+
     // Prevent page from refreshing
     event.preventDefault();
   }); // End Form submit
